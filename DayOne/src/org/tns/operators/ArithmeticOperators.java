c0fa3c86fcc634eee779329e473dec1f77dde376
SD191100 <shivamdurgude1@gmail.com>
2023-07-21 22:17:52 +0530

    FirstCommit

---
+++ DayOne/src/org/tns/operators/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+package org.tns.operators;
+
+public class ArithmeticOperators {
+    
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b; // 30
+        int d = a - b; // -10
+        int e = a * b; // 200
+        int f = a / b; // 0
+        int g = a % b; // 10
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = a + b = " + c);
+        System.out.println("d = a - b = " + d);
+        System.out.println("e = a * b = " + e);
+        System.out.println("f = a / b = " + f);
+        System.out.println("g = a % b = " + g);
+    }
+
+}
