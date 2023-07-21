e8ef80d70ed858555ff697ba2b10c020b533f49c
SD191100 <shivamdurgude1@gmail.com>
2023-07-21 22:55:08 +0530

    added Bitwise to branch-sahil

---
+++ DayOne/src/org/tns/operators/BitwiseOperator.java
@@ -0,0 +1,24 @@
+package org.tns.operators;
+
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a & b; // 0
+        int d = a | b; // 30
+        int e = a ^ b; // 30
+        int f = ~a; // -11
+        int g = a << 2; // 40
+        int h = a >> 2; // 2
+        int i = a >>> 2; // 2
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = a & b = " + c);
+        System.out.println("d = a | b = " + d);
+        System.out.println("e = a ^ b = " + e);
+        System.out.println("f = ~a = " + f);
+        System.out.println("g = a << 2 = " + g);
+        System.out.println("h = a >> 2 = " + h);
+        System.out.println("i = a >>> 2 = " + i);
+    }
+}
